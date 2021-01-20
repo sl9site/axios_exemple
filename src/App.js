95dbb2f945a8a2a96efc8c2952071b58931e6ccb
bogutskii <sl9site@gmail.com>
2021-01-19 23:43:55 -0800

    -post list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
 import PersonList from './components/PersonList'
-
+import PostPersonList from "./components/Post";
 function App() {
 
 
     return (
         <div className="App">
-
+<PostPersonList/>
 <PersonList/>
         </div>
     );

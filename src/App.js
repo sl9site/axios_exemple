79df088ba04d3692bfca08bcffbcc65b5b3361f4
bogutskii <sl9site@gmail.com>
2021-01-19 23:19:50 -0800

    - get list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
-import {getWeatherData} from './components/WeatherApi'
+import PersonList from './components/PersonList'
 
 function App() {
 
-    const [city, setCity] = useState('Lahore');
-
-
-    const getData = async () => {
-        try{
-
-            const data = await getWeatherData(city);
-
-        }catch(error) {
-            console.log(error.message);
-
-        }
-    }
 
     return (
         <div className="App">
 
-
-                    <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter your city name"/>
-                    <button type="button" onClick={() => getData()}>Search</button>
-
-
-
-
+<PersonList/>
         </div>
     );
 }

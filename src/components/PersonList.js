401ba445fc713d06c10ea29dc107903cb796b99e
bogutskii <sl9site@gmail.com>
2021-02-10 23:20:27 -0800

    class - to function get request

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,25 +1,59 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import axios from 'axios';
 
-export default class PersonList extends React.Component {
-    state = {
-        persons: []
-    }
 
-    componentDidMount() {
+const  PersonList = ()=>  {
+   const [list, setList] = useState({
+        persons: []
+    })
+
+    const componentDidMount = () => {
         axios.get(`https://jsonplaceholder.typicode.com/users`)
             .then(res => {
                 const persons = res.data;
-                this.setState({ persons });
+                console.log(res)
+                setList({ persons });
             })
     }
 
-    render() {
-        return (
+        return (<div>
+                <button onClick={componentDidMount }>get</button>
             <ul>
-                { this.state.persons.map(person => <li>{person.name}</li>)}
+                { list.persons.map(person => <li>{person.name + ' ' + person.username}</li>)}
+
             </ul>
+            </div>
         )
-    }
+
 }
+export default  PersonList;
+
+
+
+
+
+
+
+//
+// export default class PersonList extends React.Component {
+//     state = {
+//         persons: []
+//     }
+//
+//     componentDidMount() {
+//         axios.get(`https://jsonplaceholder.typicode.com/users`)
+//             .then(res => {
+//                 const persons = res.data;
+//                 console.log(res)
+//                 this.setState({ persons });
+//             })
+//     }
+//
+//     render() {
+//         return (
+//             <ul>
+//                 { this.state.persons.map(person => <li>{person.name + ' ' + person.username}</li>)}
+//             </ul>
+//         )
+//     }
+// }
